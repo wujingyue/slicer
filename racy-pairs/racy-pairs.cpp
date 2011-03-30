391efd003b77c0a27658463a61660b2b188952ad
Jingyue Wu <jingyue@jingyue.(none)>
2011-03-29 20:27:33 -0400

    added calling contexts in racy-pairs

--- racy-pairs/racy-pairs.cpp
+++ racy-pairs/racy-pairs.cpp
@@ -5,6 +5,7 @@
 #include "idm/id.h"
 #include "bc2bdd/BddAliasAnalysis.h"
 #include "common/include/util.h"
+#include "llvm-instrument/trace-manager/trace-manager.h"
 
 #include <fstream>
 #include <sstream>
@@ -12,6 +13,7 @@
 using namespace llvm;
 using namespace repair;
 using namespace std;
+using namespace tern;
 
 namespace {
 	static RegisterPass<slicer::RacyPairs> X(
@@ -19,16 +21,17 @@ namespace {
 			"Dumps all racy pairs",
 			false,
 			true); // is analysis
-	static cl::opt<string> TraceFile(
-			"trace",
-			cl::NotHidden,
-			cl::desc("The landmark trace"),
-			cl::init(""));
 	static cl::opt<string> CloneMapFile(
 			"clone-map",
 			cl::NotHidden,
-			cl::desc("The clone mapping"),
-			cl::init(""));
+			cl::ValueRequired,
+			cl::desc("The clone mapping"));
+	// We need this to get the trunk ID and therefore use the clone mapping.
+	static cl::opt<string> TraceFile(
+			"trace",
+			cl::NotHidden,
+			cl::ValueRequired,
+			cl::desc("The landmark trace"));
 	static cl::opt<bool> Cloned(
 			"cloned",
 			cl::NotHidden,
@@ -55,18 +58,18 @@ namespace slicer {
 		}
 	}
 
-	void RacyPairs::read_trace(const string &trace_file, Trace &trace) const {
-		assert(trace_file.length() > 0 && "Didn't specify -trace");
+	void RacyPairs::read_trunks(
+			const string &trace_file,
+			ThreadToTrunk &trunks) const {
 		ifstream fin(trace_file.c_str());
 		assert(fin && "Cannot open the specified landmark trace");
-		int idx = 0;
 		string line;
 		while (getline(fin, line)) {
 			istringstream iss(line);
-			int thr_id;
-			iss >> thr_id;
-			trace[thr_id].push_back(idx);
-			idx++;
+			int idx;
+			iss >> idx;
+			TraceManager &TM = getAnalysis<TraceManager>();
+			trunks[TM.get_record(idx).thr_id].push_back(idx);
 		}
 	}
 
@@ -95,15 +98,37 @@ namespace slicer {
 	}
 
 	void RacyPairs::extract_racy_pairs(
-			const InstMapping &m1,
-			const InstMapping &m2) {
+			int t1, unsigned s1, unsigned e1,
+			int t2, unsigned s2, unsigned e2,
+			const ThreadToTrunk &trunks,
+			const map<int, vector<InstMapping> > &clone_map) {
+
 		BddAliasAnalysis &BAA = getAnalysis<BddAliasAnalysis>();
-		forallconst(InstMapping, i1, m1) {
-			Instruction *ins1 = (Cloned ? i1->second : i1->first);
+		TraceManager &TM = getAnalysis<TraceManager>();
+		AddCallingContext &ACC = getAnalysis<AddCallingContext>();
+		
+		const vector<unsigned> &a1 = trunks.find(t1)->second;
+		const vector<unsigned> &a2 = trunks.find(t2)->second;
+		// p1 and p2 are global indices. 
+		// TODO: Can be optimized. a1 and a2 are ordered. 
+		for (unsigned p1 = s1; p1 < e1; ++p1) {
+			size_t tr1 = (upper_bound(a1.begin(), a1.end(), p1) - a1.begin()) - 1;
+			Instruction *ins1 = TM.get_record(p1).ins;
+			if (Cloned) {
+				Instruction *c = clone_map.find(t1)->second[tr1].lookup(ins1);
+				if (c)
+					ins1 = c;
+			}
 			if (!isa<LoadInst>(ins1) && !isa<StoreInst>(ins1))
 				continue;
-			forallconst(InstMapping, i2, m2) {
-				Instruction *ins2 = (Cloned ? i2->second : i2->first);
+			for (unsigned p2 = s2; p2 < e2; ++p2) {
+				size_t tr2 = (upper_bound(a2.begin(), a2.end(), p2) - a2.begin()) - 1;
+				Instruction *ins2 = TM.get_record(p2).ins;
+				if (Cloned) {
+					Instruction *c = clone_map.find(t2)->second[tr2].lookup(ins2);
+					if (c)
+						ins2 = c;
+				}
 				if (!isa<LoadInst>(ins2) && !isa<StoreInst>(ins2))
 					continue;
 				// If both are reads, not a race. 
@@ -115,52 +140,96 @@ namespace slicer {
 				Value *v2 = (isa<LoadInst>(ins2) ?
 						dyn_cast<LoadInst>(ins2)->getPointerOperand() :
 						dyn_cast<StoreInst>(ins2)->getPointerOperand());
-				if (BAA.alias(v1, 0, v2, 0))
+				vector<CallInst *> ctxt1, ctxt2;
+				ctxt1 = convert_context(ACC.get_calling_context(p1));
+				ctxt2 = convert_context(ACC.get_calling_context(p2));
+				if (BAA.alias(&ctxt1, v1, 0, &ctxt2, v2, 0))
 					racy_pairs.push_back(make_pair(ins1, ins2));
 			}
 		}
 	}
 
+	vector<CallInst *> RacyPairs::convert_context(const CallStack &cs) {
+		vector<CallInst *> res;
+		for (size_t i = 0, E = cs.size(); i < E; ++i) {
+			assert(isa<CallInst>(cs[i]));
+			res.push_back(dyn_cast<CallInst>(cs[i]));
+		}
+		return res;
+	}
+
+	size_t RacyPairs::find_next_enforce(
+			const vector<unsigned> &indices,
+			size_t j) const {
+		size_t k = j + 1;
+		while (k < indices.size()) {
+			TraceManager &TM = getAnalysis<TraceManager>();
+			TraceRecord record = TM.get_record(indices[k]);
+			if (record.type == TR_LANDMARK_ENFORCE)
+				break;
+			++k;
+		}
+		return k;
+	}
+
 	bool RacyPairs::runOnModule(Module &M) {
 		// Read the landmark trace. 
-		Trace trace;
-		read_trace(TraceFile, trace);
+		ThreadToTrunk trunks;
+		read_trunks(TraceFile, trunks);
 		// Read the clone mapping. 
 		map<int, vector<InstMapping> > clone_map;
 		read_clone_map(CloneMapFile, clone_map);
+		// Calculate <sync_trunks> based on <trunks>.
+		// A trunk may be bounded by non-enforcing landmarks. 
+		// A sync trunk must be bounded by enforcing landmarks (except
+		// the beginning and the end). 
+		ThreadToTrunk sync_trunks;
+		forall(ThreadToTrunk, it, trunks) {
+			int thr_id = it->first;
+			const vector<unsigned> &indices = it->second;
+			size_t j = 0;
+			while (j < indices.size()) {
+				sync_trunks[thr_id].push_back(indices[j]);
+				j = find_next_enforce(indices, j);
+			}
+			assert(sync_trunks[thr_id].size() > 0);
+			if (sync_trunks[thr_id].back() < indices.back())
+				sync_trunks[thr_id].push_back(indices.back());
+		}
 		// Collect racy pairs. 
+		// Scan sync_trunks instead of trunks. 
 		racy_pairs.clear();
-		cerr << trace.size() << endl;
-		for (Trace::iterator i1 = trace.begin(); i1 != trace.end(); ++i1) {
+		ThreadToTrunk::iterator i1, i2;
+		for (i1 = sync_trunks.begin(); i1 != sync_trunks.end(); ++i1) {
 			int t1 = i1->first;
-			Trace::iterator i2 = i1; ++i2;
-			for (; i2 != trace.end(); ++i2) {
+			for (i2 = i1, ++i2; i2 != sync_trunks.end(); ++i2) {
 				int t2 = i2->first;
-				const vector<int> &a1 = i1->second, &a2 = i2->second;
+				const vector<unsigned> &a1 = i1->second, &a2 = i2->second;
 				// j1 and j2 are trunk IDs. 
-				size_t j1 = 0, j2 = 0;
+				size_t j1 = 0;
+				size_t j2 = 0;
 				while (j1 < a1.size() && j2 < a2.size()) {
 					// Thread t1 Trunk j1: [s1, e1)
 					// Thread t2 Trunk j2: [s2, e2)
 					// If the last trunk which has only one instruction, 
 					// we virtually set e1 = s1 + 1 and e2 = s2 + 1.
-					int s1 = a1[j1], s2 = a2[j2];
-					int e1 = (j1 + 1 < a1.size() ? a1[j1 + 1] : s1 + 1);
-					int e2 = (j2 + 1 < a2.size() ? a2[j2 + 1] : s2 + 1);
+					unsigned s1 = a1[j1];
+					unsigned s2 = a2[j2];
+					unsigned e1 = (j1 + 1 < a1.size() ? a1[j1 + 1] : s1 + 1);
+					unsigned e2 = (j1 + 1 < a2.size() ? a2[j2 + 1] : s2 + 1);
 					if (e1 > s2 && e2 > s1) {
 #ifdef VERBOSE
 						cerr << "Concurrent trunks: ";
 						cerr << t1 << ":[" << s1 << ", " << e1 << ") "
 							<< t2 << ":[" << s2 << ", " << e2 << ")" << endl;
 #endif
-						// Compare trunk j1 in thread t1 and trunk j2 in thread t2. 
-						extract_racy_pairs(clone_map[t1][j1], clone_map[t2][j2]);
+						extract_racy_pairs(t1, s1, e1, t2, s2, e2, trunks, clone_map);
 					}
-					if (j1 + 1 == a1.size())
+					if (j1 + 1 < a1.size()) {
 						++j2;
-					else if (j2 + 1 == a2.size())
+					} else if (j2 + 1 < a2.size()) {
 						++j1;
-					else {
+					} else {
 						assert(j1 + 1 < a1.size() && j2 + 1 < a2.size());
 						if (a1[j1 + 1] < a2[j2 + 1])
 							++j1;
@@ -177,6 +246,7 @@ namespace slicer {
 		AU.setPreservesAll();
 		AU.addRequired<ObjectID>();
 		AU.addRequired<BddAliasAnalysis>();
+		AU.addRequired<TraceManager>();
 		ModulePass::getAnalysisUsage(AU);
 	}
 
--- racy-pairs/racy-pairs.h
+++ racy-pairs/racy-pairs.h
@@ -4,12 +4,14 @@
 #include "llvm/Module.h"
 
 #include "common/include/typedefs.h"
+#include "llvm-instrument/add-calling-context/add-calling-context.h"
 
 #include <map>
 #include <vector>
 
 using namespace llvm;
 using namespace std;
+using namespace tern;
 
 namespace slicer {
 
@@ -17,11 +19,7 @@ namespace slicer {
 
 		static char ID;
 
-		/*
-		 * Keys are thread IDs. 
-		 * The value of a key is a list of starting positions of trunks. 
-		 */
-		typedef map<int, vector<int> > Trace;
+		typedef map<int, vector<unsigned> > ThreadToTrunk;
 
 		RacyPairs(): ModulePass(&ID) {}
 		
@@ -30,12 +28,20 @@ namespace slicer {
 		virtual void print(raw_ostream &O, const Module *M) const;
 
 	private:
-		void read_trace(const string &trace_file, Trace &trace) const;
+		void read_trunks(
+				const string &trace_file,
+				ThreadToTrunk &trunks) const;
 		void read_clone_map(
 				const string &clone_map_file,
 				map<int, vector<InstMapping> > &clone_map) const;
 		void print_inst(Instruction *ins, raw_ostream &O) const;
-		void extract_racy_pairs(const InstMapping &m1, const InstMapping &m2);
+		void extract_racy_pairs(
+				int t1, unsigned s1, unsigned e1,
+				int t2, unsigned s2, unsigned e2,
+				const ThreadToTrunk &trunks,
+				const map<int, vector<InstMapping> > &clone_map);
+		size_t find_next_enforce(const vector<unsigned> &indices, size_t j) const;
+		static vector<CallInst *> convert_context(const CallStack &cs);
 
 		vector<InstPair> racy_pairs;
 	};
