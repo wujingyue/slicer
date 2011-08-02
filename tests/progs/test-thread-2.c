b4fc0c21a076bfada12085422e78654b1811b731
Jingyue Wu <jingyue@cs.columbia.edu>
2011-08-02 17:56:56 -0400

    [tests] more testcases

---
+++ tests/progs/test-thread-2.c
@@ -0,0 +1,37 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define SIZE (10)
+#define N_THREADS (2)
+
+pthread_barrier_t barrier;
+int global_arr[SIZE];
+
+void *sub_routine(void *arg) {
+	long my_num = (long)arg;
+	long i;
+
+	pthread_barrier_wait(&barrier);
+	for (i = 0; i < 5; i++) {
+		global_arr[my_num]++;
+	}
+	pthread_barrier_wait(&barrier);
+
+	return NULL;
+}
+
+int main(int argc, char *argv[]) {
+	pthread_t children[N_THREADS];
+	long i;
+
+	for (i = 0; i < SIZE; ++i)
+		global_arr[i]++;
+
+	pthread_barrier_init(&barrier, NULL, N_THREADS); 
+	for (i = 0; i < N_THREADS; ++i)
+		pthread_create(&children[i], NULL, sub_routine, (void *)(i + 1));
+	for (i = 0; i < N_THREADS; ++i)
+		pthread_join(children[i], NULL);
+
+	return 0;
+}
