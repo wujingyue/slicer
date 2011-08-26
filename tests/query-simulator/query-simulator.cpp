4654a69a8c04b478b74d1aaa07f4fbe7a4731d40
Jingyue Wu <jingyue@cs.columbia.edu>
2011-08-26 14:01:12 -0400

    Refactor:
    [reducer] Separate Constantizer and BranchRemover.

--- tests/query-simulator/query-simulator.cpp
+++ tests/query-simulator/query-simulator.cpp
@@ -19,6 +19,7 @@ using namespace llvm;
 using namespace repair;
 
 #include "int/adv-alias.h"
+#include "int/solve.h"
 #include "int/iterate.h"
 
 namespace slicer {
@@ -32,7 +33,10 @@ namespace slicer {
 	private:
 		void read_queries();
 		void issue_queries();
+		void fake_queries();
+		void analyze_queries();
 		void analyze(const Value *v);
+		Clause *construct_inst_query(unsigned iid1, unsigned iid2);
 
 		vector<QueryInfo> queries;
 	};
@@ -86,9 +90,48 @@ void QuerySimulator::read_queries() {
 }
 
 bool QuerySimulator::runOnModule(Module &M) {
+#if 0
 	read_queries();
+	analyze_queries();
 	issue_queries();
+#endif
+	fake_queries();
 	return false;
+}
+
+Clause *QuerySimulator::construct_inst_query(unsigned iid1, unsigned iid2) {
+	IDAssigner &IDA = getAnalysis<IDAssigner>();
+	Instruction *i1 = IDA.getInstruction(iid1); assert(i1);
+	Instruction *i2 = IDA.getInstruction(iid2); assert(i2);
+	assert(isa<StoreInst>(i1) || isa<LoadInst>(i1));
+	assert(isa<StoreInst>(i2) || isa<LoadInst>(i2));
+	const Value *v1 = i1->getOperand(isa<StoreInst>(i1) ? 1 : 0);
+	const Value *v2 = i2->getOperand(isa<StoreInst>(i2) ? 1 : 0);
+	return new Clause(new BoolExpr(CmpInst::ICMP_EQ,
+				new Expr(v1), new Expr(v2)));
+}
+
+void QuerySimulator::fake_queries() {
+	SolveConstraints &SC = getAnalysis<SolveConstraints>();
+
+	ifstream fin("for-jingyue/slicing.log");
+	Clause *c = NULL;
+	for (int i = 0; i < 32; ++i) {
+		unsigned iid1, iid2;
+		fin >> iid1 >> iid2;
+		if (!c)
+			c = construct_inst_query(iid1, iid2);
+		else
+			c = new Clause(Instruction::Or, c, construct_inst_query(iid1, iid2));
+	}
+
+	clock_t start = clock();
+	errs() << SC.satisfiable(c) << "\n";
+	errs() << "Time = " << clock() - start << "\n";
+
+	assert(SC.provable(c) == false);
+
+	delete c;
 }
 
 void QuerySimulator::analyze(const Value *v) {
@@ -111,23 +154,29 @@ void QuerySimulator::analyze(const Value *v) {
 		errs() << *(*itr) << "\n";
 }
 
-void QuerySimulator::issue_queries() {
-#if 1
-	AdvancedAlias &AA = getAnalysis<AdvancedAlias>();
-	SolveConstraints &SC = getAnalysis<SolveConstraints>();
-#endif
-	
+void QuerySimulator::analyze_queries() {
+	dbgs() << "Analyzing all queries...\n";
+
 	for (size_t i = 0; i < queries.size(); ++i) {
 		errs() << "Query " << i << ":\n";
 		const QueryInfo &query = queries[i];
 		analyze(query.v1);
 		analyze(query.v2);
-#if 1
+	}
+}
+
+void QuerySimulator::issue_queries() {
+	AdvancedAlias &AA = getAnalysis<AdvancedAlias>();
+	// SolveConstraints &SC = getAnalysis<SolveConstraints>();
+
+	dbgs() << "Issuing all queries...\n";
+	
+	for (size_t i = 0; i < queries.size(); ++i) {
+		const QueryInfo &query = queries[i];
 		if (query.satisfiable)
 			AA.may_alias(query.v1, query.v2);
 		else
 			AA.must_alias(query.v1, query.v2);
-#endif
 	}
 }
 
@@ -136,6 +185,7 @@ void QuerySimulator::getAnalysisUsage(AnalysisUsage &AU) const {
 	AU.addRequired<IDAssigner>();
 	AU.addRequired<Iterate>();
 	AU.addRequired<AdvancedAlias>();
+	AU.addRequired<SolveConstraints>();
 	AU.addRequired<BddAliasAnalysis>();
 	ModulePass::getAnalysisUsage(AU);
 }
