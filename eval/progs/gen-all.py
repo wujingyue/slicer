b9493fded5138f81605739a0d99f0117423cfbf3
Jingyue Wu <jingyue@cs.columbia.edu>
2011-10-19 12:00:24 -0400

    [eval] Added a script to generate all necessary bc files

---
+++ eval/progs/gen-all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import argparse
+import sys, os
+
+def invoke(cmd):
+    print >> sys.stderr, cmd
+    ret = os.system(cmd)
+    if ret != 0:
+        sys.exit(ret)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+            description = "Generate all programs to be evaluated")
+    parser.add_argument("-f",
+            help = "the configuration file (default: slicer.cfg)",
+            default = "slicer.cfg")
+    args = parser.parse_args()
+
+    assert os.path.exists(args.f)
+    config = read_config(args.f)
+    
+    for section in config.sections():
+        # Dirty
+        if section == "default" or section == "example":
+            continue
+        id_bc = section + ".bc"
+        simple_bc = section + ".simple.bc"
+        invoke("../../scripts/slicer -f " + args.f + " " + section + " " + \
+                id_bc + " " + simple_bc)
