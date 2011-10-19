d3c45690cf9052c64477a33d5ae287d2f0b8909e
Jingyue Wu <jingyue@cs.columbia.edu>
2011-10-19 15:22:21 -0400

    [eval] gen-all.py supports running only one program

--- eval/progs/gen-all.py
+++ eval/progs/gen-all.py
@@ -10,23 +10,33 @@ def invoke(cmd):
     if ret != 0:
         sys.exit(ret)
 
+def work_on(config, section):
+    assert config.has_section(section)
+    id_bc = section + ".bc"
+    simple_bc = section + ".simple.bc"
+    invoke("../../scripts/slicer -f " + args.f + " " + section + " " + \
+            id_bc + " " + simple_bc)
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
             description = "Generate all programs to be evaluated")
     parser.add_argument("-f",
             help = "the configuration file (default: slicer.cfg)",
             default = "slicer.cfg")
+    parser.add_argument("-p",
+            help = "the program to run. should match the section name in the config file",
+            default = "")
     args = parser.parse_args()
 
     assert os.path.exists(args.f)
     config = ConfigParser.ConfigParser()
     config.read(args.f)
 
-    for section in config.sections():
-        # Dirty
-        if section == "default" or section == "example":
-            continue
-        id_bc = section + ".bc"
-        simple_bc = section + ".simple.bc"
-        invoke("../../scripts/slicer -f " + args.f + " " + section + " " + \
-                id_bc + " " + simple_bc)
+    if args.p == "":
+        for section in config.sections():
+            # Dirty
+            if section == "default" or section == "example":
+                continue
+            work_on(config, section)
+    else:
+        work_on(config, args.p)
